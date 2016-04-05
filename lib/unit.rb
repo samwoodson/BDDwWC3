6bf159b278707515cfcfb4d9bde86c5088d27c45
samwoodson <samuelwoodson@gmail.com>
2016-04-05 18:51:02 +0000

    first commit

---
+++ lib/unit.rb
@@ -0,0 +1,18 @@
+class Unit
+
+  attr_reader :health_points, :attack_power
+
+  def initialize(health_points, attack_power)
+    @health_points = health_points
+    @attack_power = attack_power
+  end
+
+  def damage(attack_power)
+    @health_points -= attack_power
+  end
+
+  def attack(attack_power)
+    enemy_unit.damage(attack_power)
+  end
+
+end
